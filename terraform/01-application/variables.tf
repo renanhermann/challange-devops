d6df6bc613f8910f85c9e4d31d642972b77dbbbe
renanhermann <renanhermann@outlook.com>
2024-08-13 22:36:52 -0300

    feat: add AWS region and repository variables

---
+++ terraform/01-application/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region where the Network resources will be deployed, e.g., 'eu-central-1' for the Europe (Frankfurt) region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Deployment environment name, such as 'dev', 'test', 'prod'. This categorizes the Network resources by their usage stage"
+  type        = string
+  default     = "dev"
+}
+
+variable "repository" {
+  type = string
+  default = "renanhermann/desafio-devops"
+}
